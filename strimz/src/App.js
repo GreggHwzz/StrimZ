a55337745d5fbd489a42362a4d2aa1e7fce1fbfd
victoriaav20 <117371464+victoriaav20@users.noreply.github.com>
2023-10-09 16:26:44 +0200

    Navbar

--- strimz/src/App.js
+++ strimz/src/App.js
@@ -1,39 +1,14 @@
 import React, { useState } from 'react';
-import image from './img/Logo3_strimz.png';
-import LoginForm from './components/login/LoginForm';
-import RegisterForm from './components/register/RegisterForm'
+import NavBar from './components/Navbar';
 import './App.css';
 
 function App() {
-  const [isLoginFormOpen, setLoginFormOpen] = useState(false);
-  const [isRegisterFormOpen, setRegisterFormOpen] = useState(false);
-
-  const handleOpenLoginForm = () => {
-    setLoginFormOpen(true);
-    setRegisterFormOpen(false)
-  };
-
-  const handleCloseLoginForm = () => {
-    setLoginFormOpen(false);
-  };
-
-  const handleOpenRegisterForm = () => {
-    setRegisterFormOpen(true);
-    setLoginFormOpen(false); 
-  };
-  
-  const handleCloseRegisterForm = () => {
-    setRegisterFormOpen(false);
-  };
+ 
 
   return (
     <div >
       <header>
-        <img src={image} alt="StrimZ" className='image-logo'/>
-        <div className="header-buttons">
-          <button className='button-login' onClick={handleOpenLoginForm}>Login</button>
-          <button className='button-signup' onClick={handleOpenRegisterForm}>Sign Up</button>
-        </div>
+        <NavBar/> 
       </header>
       <main>
           <div className='main-content'>
@@ -41,20 +16,7 @@ function App() {
               <h1> Become the best music critic ! Keep track of your listenings !</h1>
             </div>
           </div>
-          {isLoginFormOpen && (
-    <div className="popup-overlay">
-      <div className="popup">
-        <LoginForm onClose={handleCloseLoginForm} />
-      </div>
-    </div>
-  )}
-  {isRegisterFormOpen && (
-    <div className="popup-overlay">
-      <div className="popup">
-        <RegisterForm onClose={handleCloseRegisterForm} />
-      </div>
-    </div>
-  )}
+          
       </main>
     </div>
   );
---
+++ strimz/src/components/Navbar.js
@@ -0,0 +1,54 @@
+import './Navbar.css';
+import React, { useState } from 'react';
+import image from '../img/Logo3_strimz.png';
+import LoginForm from './login/LoginForm';
+import RegisterForm from './register/RegisterForm'
+
+
+
+const NavBar= () => {
+const [isLoginFormOpen, setLoginFormOpen] = useState(false);
+  const [isRegisterFormOpen, setRegisterFormOpen] = useState(false);
+
+  const handleOpenLoginForm = () => {
+    setLoginFormOpen(true);
+    setRegisterFormOpen(false)
+  };
+
+  const handleCloseLoginForm = () => {
+    setLoginFormOpen(false);
+  };
+
+  const handleOpenRegisterForm = () => {
+    setRegisterFormOpen(true);
+    setLoginFormOpen(false); 
+  };
+  
+  const handleCloseRegisterForm = () => {
+    setRegisterFormOpen(false);
+  };
+    return (
+       <>
+        <img src={image} alt="StrimZ" className='image-logo'/>
+        <div className="header-buttons">
+          <button className='button-login' onClick={handleOpenLoginForm}>Login</button>
+          <button className='button-signup' onClick={handleOpenRegisterForm}>Sign Up</button>
+        </div>
+        {isLoginFormOpen && (
+    <div className="popup-overlay">
+      <div className="popup">
+        <LoginForm onClose={handleCloseLoginForm} />
+      </div>
+    </div>
+  )}
+  {isRegisterFormOpen && (
+    <div className="popup-overlay">
+      <div className="popup">
+        <RegisterForm onClose={handleCloseRegisterForm} />
+      </div>
+    </div>
+  )}
+        </>
+    )
+}
+export default NavBar;
