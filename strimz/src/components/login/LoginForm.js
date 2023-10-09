c4e60686f087d5da4d888277fafad8ee0dea4af4
GreggHwzz <gtallaapro@gmail.com>
2023-10-09 16:08:47 +0200

    Style du popup Login

--- strimz/src/components/login/LoginForm.js
+++ strimz/src/components/login/LoginForm.js
@@ -18,15 +18,13 @@ function LoginForm({onClose}) {
               &times;
             </span>
             <h2>Login</h2>
-            {/* Ajoutez ici les champs de votre formulaire */}
+            
             <form>
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" name="username" />
-
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" name="password" />
-
-              <button type="submit">Login</button>
+              <input type="text" id="username" name="username" placeholder='Username' className='login-placeholder'/>
+              <br/>
+              <input type="password" id="password" name="password" placeholder='Password' className='login-placeholder'/>
+              <br/>
+              <button type="submit" className='login-button'>Login</button>
             </form>
           </div>
         </div>
