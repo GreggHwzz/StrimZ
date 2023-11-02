b1e9cfda5a1ef3d9b76ec78b03a542cd9e0424d7
GreggHwzz <gtallaapro@gmail.com>
2023-11-02 23:59:10 +0100

    Add content to Privacy Policy

--- strimz/src/pages/PrivacyPolicy.js
+++ strimz/src/pages/PrivacyPolicy.js
@@ -1,8 +1,46 @@
 export default function PrivacyPolicy(){
     return(
-        <main>
+        <main className="content_pp">
             <div>
                 <h1>Privacy Policy</h1>
+                <h3><strong>Last Updated : November 3, 2023</strong></h3>
+                <p>Welcome to StrimZ, the web application for music enthusiasts where you can rate and discover albums from all genres. We take data privacy very seriously and are committed to protecting it. This privacy policy explains how we collect, use, share, and protect the personal data of our users.</p>
+                
+                <h2>1. Data Collection</h2>
+                <p>We collect the following information from our users:
+
+                    <br/>Personal identification information such as names and email addresses when creating an account.
+                    <br/>Musical preferences and interests to personalize the user experience.
+                    <br/>Usage and connection data to improve our services and user interface.</p>
+
+                <h2>2. Data Use</h2>
+                <p>The collected data is used to:
+
+                <br/>Provide and manage your account.
+                <br/>Enhance your user experience by personalizing the interface and musical suggestions.
+                <br/>Communicate with you, including service updates or promotional offers.</p>
+                <h2>3. Data Sharing</h2>
+                <p>StrimZ does not sell, trade, or transfer your identifiable information to third parties in any way. This does not include trusted third parties who assist us in operating our website, conducting our business, or serving our users, as long as these parties agree to keep this information confidential.</p>
+                
+                <h2>4. Storage and Security</h2>
+                <p>Personal information is stored in secure environments, and we take security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal data.</p>
+                
+                <h2>5. User Rights</h2>
+                <p>You have the right to:
+
+                <br/>Access your personal data.
+                <br/>Request the correction or deletion of your personal data.
+                <br/>Limit processing and object to data collection.
+                <br/>To exercise these rights, you can contact us at the address provided in our contact section.</p>
+                
+                <h2>6. Use of Cookies</h2>
+                <p>We use cookies to improve access to our site and identify regular visitors. In addition, our cookies enhance the user experience by tracking and targeting their interests. However, this use of cookies is in no way linked to any personally identifiable information on our site.</p>
+                
+                <h2>7. Changes to Our Privacy Policy</h2>
+                <p>Any changes to our privacy policy will be communicated on this page. We encourage you to review it regularly.</p>
+                
+                <h2>8. Contact Us</h2>
+                <p>For any questions regarding this privacy policy or your personal data, please contact us via <a href="./Contact">our contact page</a></p>
             </div>
         </main>
     )
